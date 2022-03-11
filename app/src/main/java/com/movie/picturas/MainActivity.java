c0f32265383b8e714101cac78a8caa10a24bf165
Mohammed Rahman <minhaz357@gmail.com>
2022-03-11 07:28:42 -0500

    Find views by ids

--- app/src/main/java/com/movie/picturas/MainActivity.java
+++ app/src/main/java/com/movie/picturas/MainActivity.java
@@ -3,12 +3,28 @@ package com.movie.picturas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = "MainActivity";
+
+    EditText etDescription;
+    Button btnTakePicture;
+    ImageView ivPostImage;
+    Button btnSubmit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // find the views
+        etDescription = findViewById(R.id.etDescription);
+        btnTakePicture = findViewById(R.id.btnTakePicture);
+        ivPostImage = findViewById(R.id.ivPostImage);
+        btnSubmit = findViewById(R.id.btnSubmit);
     }
 }
