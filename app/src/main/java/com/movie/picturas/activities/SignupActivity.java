74e84f28106d6ff5767fd5f54a0c437e0c882354
Mohammed Rahman <minhaz357@gmail.com>
2022-03-12 10:20:40 -0500

    Sign up user
    	- Direct user to MainActivity upon successful sign up

---
+++ app/src/main/java/com/movie/picturas/activities/SignupActivity.java
@@ -0,0 +1,85 @@
+package com.movie.picturas.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.movie.picturas.R;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
+
+import java.util.Objects;
+
+public class SignupActivity extends AppCompatActivity {
+
+    public static final String TAG = "SignupActivity";
+
+    EditText etUsername;
+    EditText etPassword;
+    Button btnSignup;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+        // Hide the action bar
+        Objects.requireNonNull(getSupportActionBar()).hide();
+
+        // find the views
+        etUsername = findViewById(R.id.etUsername);
+        etPassword = findViewById(R.id.etPassword);
+        btnSignup = findViewById(R.id.btnSignUp);
+
+        // set onClickListener to the button
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i(TAG, "onClick Sign up button");
+                String username = etUsername.getText().toString();
+                String password = etPassword.getText().toString();
+                signupUser(username, password);
+            }
+        });
+    }
+
+    private void signupUser(String username, String password) {
+        // Create the ParseUser
+        ParseUser user = new ParseUser();
+        // Set core properties
+        user.setUsername(username);
+        user.setPassword(password);
+
+        Log.i(TAG, "Attempting to signup user: "+  username);
+        // Navigate to the login activity if the user has signed in properly
+
+        user.signUpInBackground(new SignUpCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e != null){
+                    Log.e(TAG, "Signup error!" + e, e);
+                    return;
+                }
+
+                // Do I need to redirect the user to login page activity again?
+                // Or skip the login screen and direct the user to the MainActivity
+                // On successful sign up, launch MainActivity
+                launchMainActivity();
+                Toast.makeText(SignupActivity.this, "Signup Success!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    private void launchMainActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
