8d8d0f32b6fbdaa47cb82f40ab10c7edbcdcb0ba
Mohammed Rahman <minhaz357@gmail.com>
2022-03-13 19:07:05 -0400

    Create PostsAdapter to populate the RecyclerView

---
+++ app/src/main/java/com/movie/picturas/adapters/PostsAdapter.java
@@ -0,0 +1,82 @@
+package com.movie.picturas.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.movie.picturas.R;
+import com.movie.picturas.models.Post;
+import com.parse.ParseFile;
+
+import java.util.List;
+
+public class PostsAdapter extends RecyclerView.Adapter<PostsAdapter.ViewHolder>{
+
+    Context context;
+    List<Post> posts;
+
+    public PostsAdapter(Context context, List<Post> posts) {
+        this.context = context;
+        this.posts = posts;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.item_post, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Post post = posts.get(position);
+        holder.bind(post);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return posts.size();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder{
+        ImageView ivProfileImage;
+        TextView tvUsername;
+        ImageView ivImage;
+        TextView tvDescription;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            ivProfileImage = itemView.findViewById(R.id.ivProfileImage);
+            tvUsername = itemView.findViewById(R.id.tvUsername);
+            ivImage = itemView.findViewById(R.id.ivImage);
+            tvDescription = itemView.findViewById(R.id.tvDescription);
+        }
+
+        public void bind(Post post) {
+            // Bind the post data to the view elements
+            tvUsername.setText(post.getUser().getUsername());
+            tvDescription.setText(post.getDescription());
+
+            // load image
+            ParseFile image = post.getImage();
+            if (image != null){
+                Glide.with(context).load(image.getUrl()).into(ivImage);
+            }
+
+            // load profile image
+            ParseFile profileImage = post.getUser().getParseFile("profileImage");
+            if (profileImage !=null){
+                Glide.with(context).load(profileImage.getUrl()).into(ivProfileImage);
+            }
+        }
+    }
+}
